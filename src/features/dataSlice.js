7799110b2a15a7ab9802e919db287d3362879fd2
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2022-11-11 15:16:30 +0300

    tesodev task first commit

---
+++ src/features/dataSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+import dbjson from "../db.json";
+
+// data
+let db = dbjson;
+
+const data = db.data.map((info) =>
+  db.cols.map((_, i) => {
+    return [db.cols[i], info[i]];
+  })
+);
+// data to be used
+const newData = data.map((arr) => Object.fromEntries(arr));
+
+const initialState = {
+  data: newData,
+};
+
+const dataSlice = createSlice({
+  name: "dataSlice",
+  initialState,
+  reducers: {
+    addData: (state, { payload }) => {
+      state.data = payload;
+    },
+  },
+});
+
+export const dataSelect = (state) => state.dataSlice.data;
+export const { addData } = dataSlice.actions;
+export default dataSlice.reducer;
---
+++ src/store/index.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+import dataSlice from "../features/dataSlice";
+export const store = configureStore({
+  reducer: {
+    dataSlice,
+  },
+});
