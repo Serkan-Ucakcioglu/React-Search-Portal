9e0bd4522cb04c9b4be4c804703990e4ab1ae784
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2022-11-15 14:33:41 +0300

    update css

--- src/Features/Main/SearchMain.jsx
+++ src/Features/Main/SearchMain.jsx
@@ -1,11 +1,43 @@
 import React from "react";
 import logo from "../../Svg/Logo.svg";
+import Location from "../../Svg/Location.svg";
+
 function SearchMain() {
   return (
     <>
       <div className="search_head">
         <img className="logo" src={logo} alt="TESODEV-LOGO" />
         <span className="search_head_title">Search app</span>
+      </div>
+      <div className="search_content">
+        <h1>Find in records</h1>
+        <div className="search">
+          <input className="search_input" type="text" />
+          <button className="search_btn">Search</button>
+        </div>
+        <div className="search_result">
+          <div className="item">
+            <img src={Location} alt="Location-svg" />
+            <div className="item_text">
+              <span className="item_head_title">18th Street Brewery</span>
+              <span>Qakley Avenue, Hammond IN</span>
+            </div>
+          </div>
+          <div className="item">
+            <img src={Location} alt="Location-svg" />
+            <div className="item_text">
+              <span className="item_head_title">18th Street Brewery</span>
+              <span>Qakley Avenue, Hammond IN</span>
+            </div>
+          </div>
+          <div className="item">
+            <img src={Location} alt="Location-svg" />
+            <div className="item_text">
+              <span className="item_head_title">18th Street Brewery</span>
+              <span>Qakley Avenue, Hammond IN</span>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
