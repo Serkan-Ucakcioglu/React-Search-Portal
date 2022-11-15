137e29b1428074b12b0a3ce3dd88eb508b0da96f
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2022-11-15 15:09:06 +0300

    css

---
+++ src/Features/Main/TopNews.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import image from "../../Svg/Image.svg";
+import left from "../../Svg/Left.svg";
+
+function TopNews() {
+  const topsData = [
+    {
+      id: 1,
+      img: image,
+      title: "A Plan to Rebuild the Bus Terminal Everyone Loves to Hate",
+      time: "1h ago",
+      createdBy: "by Troy Corlson",
+    },
+    {
+      id: 2,
+      img: image,
+      title: "A Plan to Rebuild the Bus Terminal Everyone Loves to Hate",
+      time: "1h ago",
+      createdBy: "by Troy Corlson",
+    },
+    {
+      id: 3,
+      img: image,
+      title: "A Plan to Rebuild the Bus Terminal Everyone Loves to Hate",
+      time: "1h ago",
+      createdBy: "by Troy Corlson",
+    },
+  ];
+
+  return (
+    <div className="tops">
+      <h1 className="tops_head_title">Top News</h1>
+      <div className="news">
+        <img className="left" src={left} alt="left" />
+        {topsData?.map((news) => {
+          return (
+            <div className="news_tops">
+              <img src={news.img} alt="image" />
+              <div className="news_text">
+                <span className="news_title">{news.title}</span>
+                <span className="news_time">
+                  {news.time} ,{news.createdBy}
+                </span>
+              </div>
+            </div>
+          );
+        })}
+        <img className="right" src={left} alt="right" />
+      </div>
+    </div>
+  );
+}
+
+export default TopNews;
--- src/Views/MainPage.jsx
+++ src/Views/MainPage.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 import AddRecord from "../Features/Main/AddRecord";
 import SearchMain from "../Features/Main/SearchMain";
+import TopNews from "../Features/Main/TopNews";
 
 function MainPage() {
   return (
-    <div className="container">
-      <div className="main_wrapper">
-        <div className="add_record">
-          {" "}
-          <AddRecord />
-        </div>
-        <div className="search_main">
-          <SearchMain />
+    <>
+      <div className="container">
+        <div className="main_wrapper">
+          <div className="add_record">
+            {" "}
+            <AddRecord />
+          </div>
+          <div className="search_main">
+            <SearchMain />
+          </div>
+          <div className="top_news">
+            <TopNews />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
