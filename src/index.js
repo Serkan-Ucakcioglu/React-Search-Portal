04159dab080350c7fbde2de6488675904b4fc624
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2022-11-18 13:07:37 +0300

    update

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import store from "./Store/index";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Router/router";
+import store from "./store/index";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
