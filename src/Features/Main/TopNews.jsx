ba460b5eb83c915370e6f023fd91b5bc6bc4fe83
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2022-11-21 12:32:32 +0300

    slider work

--- src/Features/Main/TopNews.jsx
+++ src/Features/Main/TopNews.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import image from "../../Svg/Image.svg";
 import left from "../../Svg/Left.svg";
 
@@ -25,12 +25,46 @@ function TopNews() {
       time: "1h ago",
       createdBy: "by Troy Corlson",
     },
+    {
+      id: 4,
+      img: image,
+      title: "A Plan to Rebuild the Bus Terminal Everyone Loves to Hate",
+      time: "1h ago",
+      createdBy: "by Troy Corlson",
+    },
+    {
+      id: 4,
+      img: image,
+      title: "A Plan to Rebuild the Bus Terminal Everyone Loves to Hate",
+      time: "1h ago",
+      createdBy: "by Troy Corlson",
+    },
+    {
+      id: 4,
+      img: image,
+      title: "A Plan to Rebuild the Bus Terminal Everyone Loves to Hate",
+      time: "1h ago",
+      createdBy: "by Troy Corlson",
+    },
+    {
+      id: 4,
+      img: image,
+      title: "A Plan to Rebuild the Bus Terminal Everyone Loves to Hate",
+      time: "1h ago",
+      createdBy: "by Troy Corlson",
+    },
   ];
 
   const content = topsData?.map((news) => {
     return (
       <div className="news_tops">
-        <img loading="lazy" src={news?.img} alt="topnews" />
+        <img
+          loading="lazy"
+          width={327}
+          height={195}
+          src={news?.img}
+          alt="topnews"
+        />
         <div className="news_text">
           <span className="news_title">{news.title}</span>
           <span className="news_time">
@@ -40,14 +74,25 @@ function TopNews() {
       </div>
     );
   });
+  const slider = useRef();
+
+  const goLeft = () => {
+    slider.current.scrollLeft += 250;
+  };
+  const goRight = () => {
+    slider.current.scrollLeft -= 270;
+  };
 
   return (
     <div className="tops">
       <h1 className="tops_head_title">Top News</h1>
-      <div className="news">
-        <img className="left" src={left} alt="left" />
-        {content}
-        <img className="right" src={left} alt="right" />
+      <div className="content">
+        <img className="left" onClick={goLeft} src={left} alt="left" />
+
+        <div className="news" ref={slider}>
+          {content}
+        </div>
+        <img className="right" onClick={goRight} src={left} alt="right" />
       </div>
     </div>
   );
